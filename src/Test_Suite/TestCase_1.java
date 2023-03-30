2e95886660edd896dab7851a7cd34666d1c666ca
Meghana Dixit <meghamarao4240@gmail.com>
2023-03-29 19:24:29 -0700

    first commit

---
+++ src/Test_Suite/TestCase_1.java
@@ -0,0 +1,45 @@
+package Test_Suite;
+
+
+//import org.openqa.selenium.chrome.ChromeDriver;
+
+// import org.openqa.selenium.firefox.FirefoxDriver;
+ 
+
+public class TestCase_1 {
+
+	public static void main(String[] args) {
+		
+	//	Configuration with different browsers 
+		
+ // 1.Firefox browser
+		
+ /*	System.setProperty("Webdriver.gecko.driver","/Users/neerajdixit/Megh_Pre/Selenium_Prerequisites/geckodriver.exe");
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("https://www.way2automation.com/");
+*/
+		
+		
+   // 2. Chrome Browser
+		
+		
+//		System.setProperty("Webdriver.chrome.driver","/Users/neerajdixit/Megh_Pre/Selenium_Prerequisites/chromedriver");
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.get("https://www.way2automation.com/");
+//		
+		
+  // 3. Edge Browser  -- doesnt support on mac 
+		
+		
+//		System.setProperty("Webdriver.edge.driver","/Users/neerajdixit/Meghana/Selenium_Prerequisites/msedgedriver");
+//	    EdgeDriver driver = new EdgeDriver();
+//	    driver.get("https://www.way2automation.com/");
+		
+		
+		// 4. safari Browser 
+		
+	//	SafariDriver driver = new SafariDriver();
+		
+	}
+
+}
